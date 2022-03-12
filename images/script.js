b5d84c259c8cc79c2f3b99b2b166542f6355a494
KRISHNAGK9964 <kkrishna22572@gamil.com>
2022-03-12 20:59:48 +0530

    facebook done

--- images/script.js
+++ images/script.js
@@ -3,7 +3,33 @@ var darkbtn=document.getElementById("dark-btn")
 function settingsMenuToggle(){
     settingsMenu.classList.toggle("settings-menu-height")
 }
+var body=document.querySelectorAll("body");
+body.onclick=function(){
+
+    settingsMenu.classList.toggle("not:(.settings-menu-height)");
+}
 
 darkbtn.onclick=function(){
     darkbtn.classList.toggle("dark-btn-on")
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme")== "light"){
+        localStorage.setItem("theme","dark");
+    }else{
+        localStorage.setItem("theme","light");
+    }
 }
+
+if(localStorage.getItem("theme")== "light"){
+    darkbtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if(localStorage.getItem("theme")== "dark"){
+    darkbtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else{
+    localStorage.setItem("theme","light");
+}
+
+
